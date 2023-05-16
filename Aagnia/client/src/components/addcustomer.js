bf579190c60f56f8987e91c336ace2732ae99fcc
umakannan3 <umaheshcbe3@gmail.com>
2023-05-16 18:31:49 +0530

    pro11

--- Aagnia/client/src/components/addcustomer.js
+++ Aagnia/client/src/components/addcustomer.js
@@ -2,7 +2,6 @@ import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 export function Addcustomer()
 {
